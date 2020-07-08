595b4fe826ef5172bd6e7150a605b856e07aad3b
John Meade <johnnynibbles@gmail.com>
2020-07-08 18:13:55 -0400

    Validation of repeat email on complete raffle donation

--- Raffle.Web/Raffle.Web/Models/Raffle/CompleteRaffleModel.cs
+++ Raffle.Web/Raffle.Web/Models/Raffle/CompleteRaffleModel.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace Raffle.Web.Models.Raffle
 {
-    public class CompleteRaffleModel
+    public class CompleteRaffleModel : IValidatableObject
     {
+        [Required]
         public string CustomerFirstName { get; set; }
+        [Required]
         public string CustomerLastName { get; set; }
+        [EmailAddress]
         public string CustomerEmail { get; set; }
+        [EmailAddress]
         public string CustomerEmailRepeat { get; set; }
         public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
@@ -20,6 +25,16 @@ namespace Raffle.Web.Models.Raffle
         public int TotalTickets { get; set; }
 
         public List<CompleteRaffleItemModel> Items { get; set; } = new List<CompleteRaffleItemModel>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(CustomerEmail != CustomerEmailRepeat)
+            {
+                yield return new ValidationResult(
+                    $"Emails don't match.",
+                    new[] { nameof(CustomerEmailRepeat) });
+            }
+        }
     }
 
     public class CompleteRaffleItemModel
